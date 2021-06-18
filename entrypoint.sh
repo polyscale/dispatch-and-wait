d49161ac751a4438b38ab18f0ab2f59e71205ddb
Jannik Keye <jannik.keye@gmail.com>
2021-06-18 13:47:14 +0200

    fix: increase timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@ function trigger_workflow {
   echo "Triggering ${INPUT_EVENT_TYPE} in ${INPUT_OWNER}/${INPUT_REPO}"
 
   workflow_expect_runid=$(curl -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/runs?event=repository_dispatch" \
-        -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: Bearer ${INPUT_TOKEN}" | jq '.workflow_runs[0].run_number')
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer ${INPUT_TOKEN}" | jq '.workflow_runs[0].run_number')
 
   if [ "$workflow_expect_runid" = "null" ]; then
     workflow_expect_runid=0
   fi
-  workflow_expect_runid=$(( $workflow_expect_runid + 1))
+  workflow_expect_runid=$(($workflow_expect_runid + 1))
 
   resp=$(curl -X POST -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/dispatches" \
     -H "Accept: application/vnd.github.v3+json" \
@@ -16,8 +16,7 @@ function trigger_workflow {
     -H "Authorization: Bearer ${INPUT_TOKEN}" \
     -d "{\"event_type\": \"${INPUT_EVENT_TYPE}\", \"client_payload\": ${INPUT_CLIENT_PAYLOAD} }")
 
-  if [ -z "$resp" ]
-  then
+  if [ -z "$resp" ]; then
     sleep 2
   else
     echo "Workflow failed to trigger"
@@ -27,10 +26,9 @@ function trigger_workflow {
 }
 
 function ensure_workflow {
-  max_wait=10
+  max_wait=30
   stime=$(date +%s)
-  while [ $(( `date +%s` - $stime )) -lt $max_wait ]
-  do
+  while [ $(($(date +%s) - $stime)) -lt $max_wait ]; do
     workflow_runid=$(curl -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/runs?event=repository_dispatch" \
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: Bearer ${INPUT_TOKEN}" | jq ".workflow_runs[] | select(.run_number==$workflow_expect_runid) | .id")
@@ -40,7 +38,7 @@ function ensure_workflow {
   done
 
   if [ -z "$workflow_runid" ]; then
-    >&2 echo "No workflow run id found. Repository dispatch failed!"
+    echo >&2 "No workflow run id found. Repository dispatch failed!"
     exit 1
   fi
 
@@ -57,26 +55,23 @@ function wait_on_workflow {
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer ${INPUT_TOKEN}" | jq -r '.html_url'
 
-  while [[ $conclusion == "null" ]]
-  do
-    rtime=$(( `date +%s` - $stime ))
-    if [[ "$rtime" -ge "$INPUT_MAX_TIME" ]]
-    then
+  while [[ $conclusion == "null" ]]; do
+    rtime=$(($(date +%s) - $stime))
+    if [[ "$rtime" -ge "$INPUT_MAX_TIME" ]]; then
       echo "Time limit exceeded"
       exit 1
     fi
     sleep $INPUT_WAIT_TIME
     conclusion=$(curl -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/runs/${workflow_runid}" \
-    	-H "Accept: application/vnd.github.v3+json" \
-    	-H "Authorization: Bearer ${INPUT_TOKEN}" | jq -r '.conclusion')
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: Bearer ${INPUT_TOKEN}" | jq -r '.conclusion')
 
     if [ "$conclusion" == "failure" ]; then
       break
     fi
   done
 
-  if [[ $conclusion == "success" ]]
-  then
+  if [[ $conclusion == "success" ]]; then
     echo "Suceeded"
   else
     echo "Failed (conclusion: $conclusion)!"
